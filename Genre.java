2b779c0d6f47a8511eeaa3b24c9277cf5ec70e62
RaphaelCalhegas <120332582+RaphaelCalhegas@users.noreply.github.com>
2024-05-02 22:12:46 +0200

    Add files via upload

---
+++ Genre.java
@@ -0,0 +1,15 @@
+public class Genre {
+    public static final String DOCUMENTAIRES_POUR_ADULTES_ET_ENFANTS = "Documentaires pour adultes et enfants";
+    public static final String AVENTURE = "Aventure";
+    public static final String GUERRE = "Guerre";
+    public static final String HISTOIRE = "Histoire";
+    public static final String ACTION = "Action";
+    public static final String COMEDIE = "Comédie";
+    public static final String DRAME = "Drame";
+    public static final String COMEDIE_DRAMATIQUE = "Comédie dramatique";
+    public static final String FICTION_JEUNESSE = "Fiction jeunesse";
+    public static final String FILM_MUSICAL = "Film musical";
+    public static final String POLICIER_ESPIONNAGE = "Policier - espionnage";
+    public static final String SCIENCE_FICTION_FANTASTIQUE_HORREUR = "Science fiction - fantastique - horreur";
+    public static final String WESTERN = "Western";
+}
